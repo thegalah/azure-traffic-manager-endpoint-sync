46cbac9879b01a64093265343db32f8bae77f4c8
Matthew <thegalah@gmail.com>
2023-02-19 16:34:20 -0800

    Update Service.ts

--- src/Service.ts
+++ src/Service.ts
@@ -1,2 +1,5 @@
 import { TrafficManagerManagementClient } from "@azure/arm-trafficmanager";
 import { DefaultAzureCredential } from "@azure/identity";
+
+const subscriptionId = "00000000-0000-0000-0000-000000000000";
+const client = new TrafficManagerManagementClient(new DefaultAzureCredential(), subscriptionId);
